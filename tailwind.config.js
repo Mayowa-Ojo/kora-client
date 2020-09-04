6cd8d259f005a1c072ca99863578a3d5214b028a
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-09-04 00:00:10 -0600

    :wrench: [config]: extend colors and font-size defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+   purge: [],
+   theme: {
+      extend: {
+         colors: {
+            kora: {
+               dark1: "#292E39",
+               dark2: "#313742",
+               dark3: "#3B414B",
+               light1: "#E0E6F0",
+               blue1: "#3498DB",
+               blue2: "#2296F3",
+               red1: "#EB5757"
+            }
+         },
+         fontSize: {
+            "k-13": ".81rem",
+            "k-14": ".88rem",
+            "k-15": ".94rem",
+            "k-18": "1.13rem",
+            "k-21": "1.31rem",
+            "k-24": "1.5rem"
+         }
+      },
+   },
+   variants: {},
+   plugins: [],
 }
