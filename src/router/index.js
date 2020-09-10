2cb98cea5069b1d0f815732123a397975bfbab65
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-09-10 09:19:35 -0600

    :sparkles: [feat]: add /notifications route

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,17 @@ const routes = [
       component: Home
    },
    {
-      path: '/about',
-      name: 'About',
+      path: '/answer',
+      name: 'Answer',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ '../views/Answer.vue')
+   },
+   {
+      path: '/notifications',
+      name: 'Notifications',
+      component: () => import(/* webpackChunkName: "about" */ '../views/Notifications.vue')
    }
 ]
 
