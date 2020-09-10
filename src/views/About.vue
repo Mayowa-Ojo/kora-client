561412a912433d2ab4f769ceb24bf9d4f5ce3921
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-09-10 09:17:31 -0600

    :construction: [feat]: work in progress

--- src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1 class="text-lg text-blue-400 text-center">This is an about page</h1>
-  </div>
-</template>
--- src/views/Answer.vue
+++ src/views/Answer.vue
@@ -7,36 +7,69 @@
          <p class="mt-1 -mx-4 py-1 px-4 text-kora-light1 text-k-13 font-normal cursor-pointer hover:bg-kora-dark2 rounded">Answer drafts</p>
       </aside>
 
-      <div class="q-box--center border-light-25">
-         <div class="bg-kora-dark2 py-2 px-4 flex border-b border-kora-light1 border-opacity-10">
-            <span class="inline-block p-1 bg-kora-red1 rounded-sm">
-               <svg width="12" height="12" viewBox="0 0 12 12" fill="none">
-                  <path d="M5.35626 0.399536L3.89159 3.36925L0.614589 3.84701C0.0269265 3.93224 -0.208587 4.65672 0.21758 5.07168L2.58842 7.38195L2.02767 10.6455C1.92674 11.2354 2.54804 11.6773 3.06842 11.4014L6 9.86045L8.93158 11.4014C9.45196 11.675 10.0733 11.2354 9.97233 10.6455L9.41158 7.38195L11.7824 5.07168C12.2086 4.65672 11.9731 3.93224 11.3854 3.84701L8.10841 3.36925L6.64374 0.399536C6.38131 -0.129809 5.62093 -0.136538 5.35626 0.399536Z" fill="#E0E6F0"/>
-               </svg>
-            </span>
-            <p class="ml-2 text-kora-light1 text-k-13 font-light hover:underline cursor-pointer">Questions For You</p>
+      <div class="q-box--center ">
+         <div class="mb-4 border-light-25 rounded-sm">
+            <div class="bg-kora-dark2 py-2 px-4 flex border-b border-kora-light1 border-opacity-10">
+               <span class="inline-block p-1 bg-kora-red1 rounded-sm">
+                  <Icon :classes="'w-3 h-3 fill-current text-kora-gray1'" :viewbox="getIcons['star'].viewbox" :path="getIcons['star'].path" />
+               </span>
+               <p class="ml-2 text-kora-light1 text-k-13 font-light hover:underline cursor-pointer">Questions For You</p>
+            </div>
+
+            <QuestionPreview 
+               v-for="i in 10"
+               :key="i"
+            />
+
+            <div class="h-8 py-1 flex justify-center items-center">
+               <span class="py-1 px-2 inline-flex items-center cursor-pointer hover:bg-kora-dark3 rounded-full">
+                  <p class="mx-1 text-kora-light1 text-k-13 font-medium">More</p>
+                  <span class="p-1 mt-1 inline-block">
+                     <Icon 
+                        :class="'fill-current text-kora-gray1'" 
+                        :viewbox="getIcons['chevron'].viewbox" 
+                        :path="getIcons['chevron'].path" 
+                        :width="getIcons['chevron'].width" 
+                        :height="getIcons['chevron'].height" 
+                     />
+                  </span>
+               </span>
+            </div>
          </div>
 
-         <QuestionPreview 
-            v-for="i in 1"
-            :key="i"
-         />
+         <div class="px-6 py-4 flex justify-center">
+            <ScrollEnd />
+         </div>
       </div>
 
       <aside class="q-box--right">
-
+         <div class="w-full border-light-25 p-4 rounded-sm">
+            <p class="mb-2 text-kora-light1 text-k-15 font-medium">Add topics you know about</p>
+            <p class="mb-2 text-kora-light1 text-k-13 font-light">Adding topics helps us find questions for you to answer</p>
+            <button 
+               class="mt-2 px-4 py-1 rounded-full border-light-25 bg-kora-dark2 text-kora-red1 text-k-13 font-medium hover:bg-kora-dark3" 
+               type="button"
+            >Add topics
+            </button>
+         </div>
       </aside>
    </div>
 </template>
 
 <script>
 import QuestionPreview from "../components/QuestionPreview";
+import ScrollEnd from "../components/ScrollEnd"
+import Icon from "../components/Icon";
+import { iconsMixin } from "../utils/mixins";
 
 export default {
    name: "Answer",
    components: {
-      QuestionPreview
-   }
+      QuestionPreview,
+      ScrollEnd,
+      Icon
+   },
+   mixins: [iconsMixin]
 };
 </script>
 
@@ -45,6 +78,10 @@ export default {
    &--center {
       margin-left: 30px;
       width: 602px;
+   }
+   &--right {
+      width: 172px;
+      margin-left: 56px;
    }
 }
 </style>
