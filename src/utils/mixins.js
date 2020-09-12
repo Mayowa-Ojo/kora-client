9f356cac04f2c1af11b8a55d0049c1ed055af442
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-09-12 13:31:07 -0600

    :sparkles: [feat]: create shortId mixin

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -1,7 +1,14 @@
 import { icons } from "../constants/icons";
+import { generateUniqueId } from "../utils/common";
 
 export const iconsMixin = {
    computed: {
       getIcons: () => icons
+   }
+}
+
+export const shortidMixin = {
+   methods: {
+      generateId: () => generateUniqueId()
    }
 }
