7841e3f597f7281f97e266e6f138da54a281e303
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-09-17 16:01:47 -0600

    :sparkles: [feat]: update, ma fo!

---
+++ src/components/ModalWrapper.vue
@@ -0,0 +1,50 @@
+<template>
+   <portal>
+      <div 
+         class="w-screen h-screen fixed inset-0 bg-kora-gray1 bg-opacity-50 flex justify-center items-center z-30"
+         v-if="$store.getters.modalActive"
+         v-on:click.self="handleCloseModal"
+      >
+         <component 
+            :is="$store.getters.currentModal"
+            v-on:close-modal="handleCloseModal"
+         >
+         </component>
+      </div>
+   </portal>
+</template>
+
+<script>
+import { Portal, setSelector } from "@linusborg/vue-simple-portal";
+import AddQuestionModal from "./AddQuestionModal";
+import AddTopicModal from "./AddTopicModal";
+import { ACTIONS } from '../constants/store';
+
+setSelector("modal-target");
+
+export default {
+   name: "ModalWrapper",
+   components: {
+      Portal,
+      AddQuestionModal,
+      AddTopicModal
+   },
+   methods: {
+      handleCloseModal: function() {
+         this.$store.dispatch(ACTIONS.TOGGLE_MODAL);
+      }
+   },
+   mounted: function() {
+      this.$store.watch((_state, getters) => {
+         return getters.modalActive;
+      }, (newVal) => {
+         if(newVal) {
+            document.body.style.overflow = "hidden";
+         } else {
+            document.body.style.overflow = "auto";
+         }
+      });
+   }
+}
+
+</script>
