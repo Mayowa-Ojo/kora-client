627e47ee60f8a69078cd067e0d311f1a8a440869
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-09-18 07:27:46 -0600

    :sparkles: [feat]: create modal and data mixins

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -1,5 +1,7 @@
 import { icons } from "../constants/icons";
+import { spaces, topics } from "../tmp/db";
 import { generateUniqueId } from "../utils/common";
+import { ACTIONS} from "../constants/store";
 
 export const iconsMixin = {
    computed: {
@@ -10,5 +12,23 @@ export const iconsMixin = {
 export const shortidMixin = {
    methods: {
       generateId: () => generateUniqueId()
+   }
+}
+
+export const dataMixin = {
+   computed: {
+      getTempData: () => ({ spaces, topics })
+   }
+}
+
+export const modalMixin = {
+   methods: {
+      toggleModal: function(component) {
+         const payload = {
+            component
+         }
+
+         this.$store.dispatch(ACTIONS.TOGGLE_MODAL, payload);
+      }
    }
 }
