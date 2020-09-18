84ac66e5c80e92a804a8daa7d1dc92a38f952e2d
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-09-18 07:26:18 -0600

    :lipstick: [style]: adjust horizontal alignment

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -5,15 +5,17 @@
             <h1 class="text-kora-light1 text-k-18 font-bold mb-1">Page Not Found</h1>
             <p class="text-kora-light1 text-k-13 font-normal">We searched everywhere but couldn't find the page you were looking for.</p>
             <div class="flex items-center pt-5">
-               <span 
-                  class="text-kora-blue1 text-k-13 font-normal cursor-pointer hover:underline"
-                  @click="$router.go(-1)"
-                  >
-                  Go Back
-               </span>
-               <span class="dot-separator"></span>
+               <router-link :to="{}">
+                  <span 
+                     class="text-kora-blue1 text-k-13 font-normal cursor-pointer hover:underline"
+                     @click="$router.go(-1)"
+                     >
+                     Go Back
+                  </span>
+               </router-link>
+               <span class="dot-separator mt-1"></span>
                <router-link :to="{ path: '/' }">
-               <span class="text-kora-blue1 text-k-13 font-normal cursor-pointer hover:underline">Kora Home</span>
+                  <span class="text-kora-blue1 text-k-13 font-normal cursor-pointer hover:underline">Kora Home</span>
                </router-link>
             </div>
          </div>
