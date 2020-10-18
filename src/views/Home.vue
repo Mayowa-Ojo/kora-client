dcb2be059f6cc7714a9bf24aa6edbac4ef71c384
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-10-17 22:54:26 -0600

    :lipstick: [style]: refactor ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -66,7 +66,10 @@
       </aside>
 
       <div class="q-box--center">
-         <div class="h-20 p-4 mb-2 bg-kora-dark2 rounded border-light-25 cursor-pointer">
+         <div 
+            class="h-20 p-4 mb-2 bg-kora-dark2 rounded border-light-25 cursor-pointer"
+            @click="toggleModal('AddQuestionModal')"
+         >
             <div class="flex mb-2 items-center">
                <span class="inline-flex items-center justify-center w-5 h-5 mr-2 bg-kora-blue1 rounded-full hover:opacity-75">
                   <Icon 
@@ -91,15 +94,19 @@
                   <span class="dot-separator"></span>
                   <p class="text-kora-light1 text-k-13 font-light">Recommended for you</p>
                </div>
-               <span class="inline-block w-4 h-4 cursor-pointer">
-                  <Icon 
-                     :class="'fill-current text-kora-light1'" 
-                     :viewbox="getIcons['x'].viewbox" 
-                     :path="getIcons['x'].path" 
-                     :width="getIcons['x'].width" 
-                     :height="getIcons['x'].height" 
-                  />
-               </span>
+               <Tooltip :offset="12" :placement="'top'" :text="'Hide'">
+                  <template v-slot:trigger>
+                     <span class="trigger inline-flex justify-center items-center w-8 h-8 rounded-full cursor-pointer hover:bg-kora-dark1 hover:bg-opacity-50">
+                        <Icon 
+                           :class="'fill-current text-kora-red1'" 
+                           :viewbox="getIcons['x'].viewbox" 
+                           :path="getIcons['x'].path" 
+                           :width="getIcons['x'].width" 
+                           :height="getIcons['x'].height" 
+                        />
+                     </span>
+                  </template>
+               </Tooltip>
             </div>
             <div class="flex justify-between">
                <div class="flex">
@@ -117,15 +124,19 @@
                      </div>
                   </div>
                </div>
-               <span class="inline-flex w-6 h-6 bg-kora-blue1 rounded-full border-light-25 overflow-hidden items-center justify-center cursor-pointer">
-                  <Icon 
-                     :class="'fill-current text-kora-light1'" 
-                     :viewbox="getIcons['follow'].viewbox" 
-                     :path="getIcons['follow'].path" 
-                     :width="getIcons['follow'].width" 
-                     :height="getIcons['follow'].height" 
-                  />
-               </span>
+               <Tooltip :offset="12" :placement="'top'" :text="'Follow Gary Simmons'">
+                  <template v-slot:trigger>
+                  <span class="trigger inline-flex w-6 h-6 bg-kora-gray1 rounded-full border-light-25 overflow-hidden items-center justify-center cursor-pointer hover:bg-opacity-50">
+                     <Icon 
+                        :class="'fill-current text-kora-dark2'" 
+                        :viewbox="getIcons['follow'].viewbox" 
+                        :path="getIcons['follow'].path" 
+                        :width="getIcons['follow'].width" 
+                        :height="getIcons['follow'].height" 
+                     />
+                  </span>
+                  </template>
+               </Tooltip>
             </div>
             <!-- rich text content -->
             <div class="mb-2">
@@ -214,6 +225,79 @@
                   </Tooltip>
                </div>
             </div>
+         </div>
+
+         <div class="discover mb-2 border-light-25 rounded">
+            <div class="header p-4 flex items-center border-b border-kora-light1 border-opacity-10">
+               <span class="inline-flex items-center justify-center w-6 h-6 rounded-sm bg-kora-red1">
+                  <Icon 
+                     :class="'fill-current inline-block text-kora-dark2 w-4 h-4'" 
+                     :viewbox="getIcons['telescope'].viewbox" 
+                     :path="getIcons['telescope'].path" 
+                  />
+               </span>
+               <p class="ml-2 text-k-13 font-normal text-kora-light1 capitalize">discover new spaces</p>
+            </div>
+            <div 
+               class="flex px-4 py-2 cursor-pointer border-b border-kora-light1 border-opacity-10"
+               v-for="i in 4"
+               :key="i"
+            >
+               <span class="inline-block w-10 h-10 rounded-full overflow-hidden">
+                  <img src="https://qph.fs.quoracdn.net/main-thumb-ti-1694992-100-clncvqrqpvglkplgwpyldayvskjuaizh.jpeg" alt="space avatar">
+               </span>
+               <div class="flex flex-col ml-4 flex-auto">
+                  <p class="text-k-15 font-medium text-kora-light1">Physics and Chemistry</p>
+                  <p class="text-k-13 font-normal text-kora-light1 text-opacity-50">112.2K Followers</p>
+                  <p class="text-k-13 font-normal text-kora-light1 mt-2">All about Physics and Chemistry</p>
+               </div>
+               <div class="h-full flex items-start py-2">
+                  <button type="button" class="px-3 py-1 rounded-full text-k-13 font-medium text-kora-dark2 bg-kora-gray1 hover:bg-opacity-75">
+                     <Icon 
+                        :class="'fill-current inline-block text-kora-dark2 w-4 h-4 mr-1'" 
+                        :viewbox="getIcons['follow'].viewbox" 
+                        :path="getIcons['follow'].path" 
+                     />
+                     Follow
+                  </button>
+                  <span class="inline-flex items-center justify-center -mt-1 ml-3 w-8 h-8 rounded-full cursor-pointer hover:bg-kora-dark2">
+                     <Icon 
+                        :class="'fill-current inline-block text-kora-red1'" 
+                        :viewbox="getIcons['x'].viewbox" 
+                        :path="getIcons['x'].path" 
+                        :width="getIcons['x'].width" 
+                        :height="getIcons['x'].height" 
+                     />
+                  </span>
+               </div>
+            </div>
+            <div class="flex items-center justify-center h-10 cursor-pointer hover:bg-kora-dark2">
+               <p class="text-k-14 font-normal text-kora-light1 capitalize">view more spaces</p>
+               <Icon 
+                  :class="'fill-current inline-block text-kora-light1 w-3 h-3 ml-2 transform -rotate-90'" 
+                  :viewbox="getIcons['chevron'].viewbox" 
+                  :path="getIcons['chevron'].path" 
+               />
+            </div>
+         </div>
+
+         <div class="suggested-questions mb-2 border-light-25 rounded">
+            <div class="header p-4 flex items-center border-b border-kora-light1 border-opacity-10">
+               <Tooltip :offset="12" :placement="'top'" :text="'Hide'">
+                  <template v-slot:trigger>
+                     <span class="trigger inline-flex items-center justify-center w-6 h-6 rounded-sm bg-kora-red1">
+                        <Icon 
+                           :class="'fill-current inline-block text-kora-dark2 w-4 h-4'" 
+                           :viewbox="getIcons['lightbulb'].viewbox" 
+                           :path="getIcons['lightbulb'].path" 
+                        />
+                     </span>
+                  </template>
+               </Tooltip>
+               <p class="ml-2 text-k-13 font-normal text-kora-light1">Questions for you</p>
+            </div>
+            <QuestionPreview />
+            <QuestionPreview />
          </div>
       </div>
 
@@ -306,16 +390,18 @@
 <script>
 import Icon from "../components/Icon";
 import Tooltip from "../components/Tooltip";
+import QuestionPreview from "../components/QuestionPreview";
 import { spaces, topics } from "../tmp/db";
-import { shortidMixin, iconsMixin } from "../utils/mixins";
+import { shortidMixin, iconsMixin, modalMixin } from "../utils/mixins";
 
 export default {
    name: "Home",
    components: {
       Tooltip,
-      Icon
+      Icon,
+      QuestionPreview
    },
-   mixins: [shortidMixin, iconsMixin],
+   mixins: [shortidMixin, iconsMixin, modalMixin],
    computed: {
       tmpdata: function() {
          return {
