2de2fb6450568a17108427496835e5969cbf8d1c
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-08 22:34:24 -0700

    :recycle: [refactor]: move action-bar markup to component

--- src/components/AnswerPreview.vue
+++ src/components/AnswerPreview.vue
@@ -1,9 +1,12 @@
 <template>
    <div 
-      class="profile-feed--item pt-4 pb-2"
+      class="pt-4 pb-2"
       :class="{ 'border-bottom': options.borderBottom }"
    >
-      <span class="text-kora-light1 text-k-13 font-normal text-opacity-50 inline-flex items-center mb-2">
+      <span 
+         class="text-kora-light1 text-k-13 font-normal text-opacity-50 inline-flex items-center mb-2"
+         v-if="options.pinned"
+      >
          <Icon 
             :class="'fill-current text-kora-light1 mr-2 opacity-50'" 
             :viewbox="getIcons['pin'].viewbox" 
@@ -27,91 +30,42 @@
                   <p class="text-kora-light1 text-k-13 font-light">Experienced in C, C++ and Java</p>
                </span>
             </div>
-            <span 
-               class="inline-flex items-center justify-center w-8 h-8 bg-kora-dark3 rounded-full cursor-pointer hover:opacity-75"
-               :class="[!options.followIcon ? 'invisible' : 'visible']"
-            >
-               <Icon 
-                  :class="'fill-current text-kora-gray1'" 
-                  :viewbox="getIcons['following'].viewbox" 
-                  :path="getIcons['following'].path" 
-                  :width="getIcons['following'].width" 
-                  :height="getIcons['following'].height" 
-               />
-            </span>
+            <Tooltip :offset="8" :placement="'top'" :text="'Following Alan Mellor'" v-if="options.followIcon">
+               <template v-slot:trigger>
+                  <span class="trigger inline-flex items-center justify-center w-8 h-8 bg-kora-dark3 rounded-full cursor-pointer hover:opacity-75">
+                     <Icon 
+                        :class="'fill-current text-kora-blue1'" 
+                        :viewbox="getIcons['following'].viewbox" 
+                        :path="getIcons['following'].path" 
+                        :width="getIcons['following'].width" 
+                        :height="getIcons['following'].height" 
+                     />
+                  </span>
+               </template>
+            </Tooltip>
          </div>
          <div class="mb-2">
-            <p class="text-kora-light1 text-k-15 font-bold cursor-pointer hover:underline">How do I learn object-oriented programming?</p>
-            <p class="text-kora-light1 text-k-15 font-normal cursor-pointer">
-               I think of all the objects as little people at work, each one having a job to do. Each one knows how to do their job, and has all the reference books and supplies they need to do it. So we might have a writer, who writes a letter. This letter is then handed to a... 
-               <span class="text-kora-blue1 hover:underline">(more)</span>
-            </p>
-         </div>
-         <div class="flex items-center justify-between -ml-2">
-            <div class="flex items-center">
-               <Tooltip :offset="8" :placement="'top'" :text="'Upvote'">
-                  <template v-slot:trigger>
-                     <span class="trigger mr-2 text-kora-light1 text-k-13 font-light py-1 px-2 inline-flex items-center rounded-full cursor-pointer hover:bg-kora-dark3 hover:bg-opacity-50">
-                        <Icon 
-                           :class="'w-4 h-4 mr-1 fill-current text-kora-light1'" 
-                           :viewbox="getIcons['upvote'].viewbox" 
-                           :path="getIcons['upvote'].path" 
-                        />
-                        103
-                     </span>
-                  </template>
-               </Tooltip>
-               <Tooltip :offset="8" :placement="'top'" :text="'Share'">
-                  <template v-slot:trigger>
-                     <span 
-                        class="trigger mr-2 text-kora-light1 text-k-13 font-light py-1 px-2 inline-flex items-center rounded-full cursor-pointer hover:bg-kora-dark3 hover:bg-opacity-50"
-                        @click="toggleModal('SharePostModal')"
-                     >
-                        <Icon 
-                           :class="'w-4 h-4 mr-2 fill-current text-kora-light1'" 
-                           :viewbox="getIcons['share'].viewbox" 
-                           :path="getIcons['share'].path" 
-                        />
-                        2
-                     </span>
-                  </template>
-               </Tooltip>
-               <Tooltip :offset="8" :placement="'top'" :text="'Comment'">
-                  <template v-slot:trigger>
-                     <span class="trigger mr-2 text-kora-light1 text-k-13 font-light py-1 px-2 inline-flex items-center rounded-full cursor-pointer hover:bg-kora-dark3 hover:bg-opacity-50">
-                        <Icon 
-                           :class="'w-5 h-5 mr-1 stroke-current text-kora-light1'" 
-                           :viewbox="getIcons['comment'].viewbox" 
-                           :path="getIcons['comment'].path" 
-                        />
-                        13
-                     </span>
-                  </template>
-               </Tooltip>
+            <p class="text-kora-light1 text-k-15 font-bold cursor-pointer hover:underline" v-if="options.title">How do I learn object-oriented programming?</p>
+            <div class="content">
+               <p class="text-kora-light1 text-k-15 font-normal cursor-pointer">
+                  Engineers have a low opinion of everyone. Marketing is just a bunch of buzzwords, sales is a bunch of back slappers and HR is full of robots. Accounting is alright - at least they can do math. Also, managers really are pretty bad on average. There's no good way to try management without becoming a ma
+               </p>
+               <span class="text-kora-blue1 text-k-15 font-normal cursor-pointer hover:underline">... (more)</span>
             </div>
-            <div class="flex items-center">
-               <Tooltip :offset="8" :placement="'top'" :text="'More sharing options'">
-                  <template v-slot:trigger>
-                     <span class="trigger w-8 h-8 mr-2 inline-flex justify-center items-center rounded-full cursor-pointer hover:bg-kora-dark3 hover:bg-opacity-50">
-                        <Icon 
-                           :class="'w-4 h-4 fill-current text-kora-light1'" 
-                           :viewbox="getIcons['shareAlt'].viewbox" 
-                           :path="getIcons['shareAlt'].path" 
-                        />
-                     </span>
-                  </template>
-               </Tooltip>
-               <Tooltip :offset="8" :placement="'top'" :text="'More'">
-                  <template v-slot:trigger>
-                     <span class="trigger w-8 h-8 inline-flex justify-center items-center rounded-full cursor-pointer hover:bg-kora-dark3 hover:bg-opacity-50">
-                        <Icon 
-                           :class="'w-5 h-5 fill-current text-kora-light1'" 
-                           :viewbox="getIcons['kebabMenu'].viewbox" 
-                           :path="getIcons['kebabMenu'].path" 
-                        />
-                     </span>
-                  </template>
-               </Tooltip>
+         </div>
+         <div class="flex items-center">
+            <span class="text-k-13 font-normal text-kora-light1 text-opacity-50">77 views</span>
+            <span class="dot-separator"></span>
+            <span class="text-k-13 font-normal text-kora-light1 text-opacity-50 cursor-pointer hover:underline">View Upvoters</span>
+            <span class="dot-separator"></span>
+            <span class="text-k-13 font-normal text-kora-light1 text-opacity-50 cursor-pointer hover:underline">View Sharers</span>
+         </div>
+
+         <ActionBar :target="'post'" :options="options" />
+
+         <div class="mt-2">
+            <div class="px-4 py-2 bg-kora-dark2 border-light-25 rounded">
+               <CommentInput />
             </div>
          </div>
       </div>
@@ -121,20 +75,42 @@
 <script>
 import Icon from "../components/Icon";
 import Tooltip from "../components/Tooltip";
+import ActionBar from "../components/ActionBar";
+import CommentInput from "../components/CommentInput";
 import { iconsMixin, modalMixin } from "../utils/mixins";
 
 export default {
    name: "AnswerPreviewProfile",
    components: {
       Icon,
-      Tooltip
+      Tooltip,
+      ActionBar,
+      CommentInput
    },
    props: ["options"],
-   mixins: [iconsMixin, modalMixin]
+   mixins: [iconsMixin, modalMixin],
+   data: () => ({
+      truncateContent: true
+   })
 }
 
 </script>
 
 <style lang="scss" scoped>
-
+.content {
+   display: -webkit-box;
+   overflow: hidden;
+   text-overflow: ellipsis;
+   -webkit-box-orient: vertical;
+   -webkit-line-clamp: 3;
+   position: relative;
+   span {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      line-height: 1.4rem;
+      background: #292E39;
+      padding-left: 2px;
+   }
+}
 </style>
