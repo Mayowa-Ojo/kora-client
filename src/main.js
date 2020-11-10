693d50047d55c31d97120ff4dbc09d4fb9e94684
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-10 15:31:28 -0700

    :sparkles: [feat]: import datastore

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "./assets/css/app.css"
+import "./services/nedb";
 
 Vue.config.productionTip = false
 
