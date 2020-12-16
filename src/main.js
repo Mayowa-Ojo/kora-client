46ec7520066c7485bdea019f29d01f2df16b55cc
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-16 12:45:12 -0700

    :recycle: [refactor]: remove unused import

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "./assets/css/app.css"
-import "./services/nedb";
 
 Vue.config.productionTip = false
 
