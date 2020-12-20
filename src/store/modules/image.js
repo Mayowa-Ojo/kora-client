5ecce7f106058a48e8776c298fcfc1fca571cc25
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-19 23:52:52 -0700

    :recycle: [refactor]: use central error handler

--- src/store/modules/image.js
+++ src/store/modules/image.js
@@ -1,5 +1,4 @@
 import { ACTIONS, MUTATIONS } from "../../constants/store";
-import apiRoutes from "../../constants/apiRoutes";
 import httpRequest from "../../services/http";
 
 export default {
@@ -7,36 +6,22 @@ export default {
    mutations: {},
    actions: {
       [ACTIONS.UPLOAD_IMAGE]: async function({ commit }, payload) {
-         try {
-            commit(MUTATIONS.SET_STATUS, "loading");
+         commit(MUTATIONS.SET_STATUS, "loading");
 
-            const formData = new FormData();
-            formData.append("image", payload.file);
+         const formData = new FormData();
+         formData.append("image", payload.file);
 
-            const options = {
-               method: "POST",
-               data: formData
-            }
-
-            const { data: response } = await httpRequest(apiRoutes.image.create, options);
-
-            if(!response.ok) {
-               //TODO: implememt proper error handling [i.e UI should indicate errors state]
-               commit(MUTATIONS.SET_STATUS, "error");
-
-               return;
-            }
-
-            console.log("[LOG]: data - \n", response);
-            commit(MUTATIONS.SET_STATUS, "done");
-
-            return response;
-         } catch(err) {
-            commit(MUTATIONS.SET_STATUS, "error");
-            console.error("[ERROR]: ", err);
+         const options = {
+            method: "POST",
+            data: formData
          }
 
+         const response = await httpRequest("/images", options);
 
+         console.log("[LOG]: data - \n", response);
+         commit(MUTATIONS.SET_STATUS, "done");
+
+         return response;
       }
    },
    getters: {}
