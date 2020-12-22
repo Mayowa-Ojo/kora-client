29a5b92535242f190f450a79720c6a0366ff6a1d
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-21 21:39:45 -0700

    :sparkles: [feat]: implement submit handler

--- src/components/AddSpaceModal.vue
+++ src/components/AddSpaceModal.vue
@@ -28,8 +28,9 @@
                class="w-full appearance-none bg-transparent text-kora-light1 text-k-15 font-normal focus:outline-none"
                type="text"
                placeholder="Enter a name for your Space"
+               v-model="name"
+               maxlength="40"
             >
-            <!-- NOTE: display text field limit -->
             <span></span>
          </div>
          <div class="field-validation mt-2 h-4">
@@ -38,18 +39,28 @@
          <div class="mt-4 py-2">
             <p class="text-kora-light1 text-k-15 font-medium">About</p>
          </div>
-         <div class="text-input border-b border-kora-light1 border-opacity-10 flex items-center justify-between">
+         <div class="text-input pr-6 border-b border-kora-light1 border-opacity-10 flex items-center justify-between relative">
             <input 
                class="w-full appearance-none bg-transparent text-kora-light1 text-k-15 font-normal focus:outline-none"
                type="text"
                placeholder="1-line description of your Space"
+               v-model="about"
+               maxlength="80"
             >
+            <span
+               class="absolute text-opacity-75 text-k-13 font-normal bottom-0 right-0"
+               :class="[about.length < 65 ? 'text-kora-light1' : 'text-red-400']"
+            >{{80 - about.length}}</span>
          </div>
       </div>
 
       <div class="modal-dialog__footer p-3 flex justify-end bg-kora-dark2 border-t border-kora-light1 border-opacity-10">
          <div>
-            <button class="py-1 px-4 rounded-full text-kora-dark2 text-k-14 font-medium bg-kora-red1 focus:outline-none">
+            <button
+               class="py-1 px-4 rounded-full text-kora-dark2 text-k-14 font-medium bg-kora-red1 focus:outline-none"
+               :disabled="name.length < 1 || about.length < 1"
+               @click="handleSubmit"
+            >
                Done
             </button>
          </div>
@@ -60,13 +71,39 @@
 <script>
 import Icon from "./Icon";
 import { iconsMixin } from "../utils/mixins";
+import { ACTIONS } from "../constants/store";
 
 export default {
    name: "AddSpaceModal",
    components: {
       Icon
    },
-   mixins: [iconsMixin]
+   mixins: [iconsMixin],
+   data: () => ({
+      name: "",
+      about: ""
+   }),
+   methods: {
+      handleSubmit: async function() {
+         const payload = {
+            data: {
+               name: this.name,
+               about: this.about,
+            }
+         }
+
+         this.$store.dispatch(ACTIONS.TOGGLE_MODAL);
+
+         const response = await this.$store.dispatch(ACTIONS.CREATE_SPACE, payload);
+
+         if(this.$store.state.status == "error") {
+            // handle error message
+            return;
+         }
+
+         this.$router.push(`/space/${response.data.slug}`);
+      }
+   }
 }
 
 </script>
@@ -82,6 +119,9 @@ export default {
       .text-input {
          min-height: 45px;
       }
+   }
+   button[disabled] {
+      cursor: default;
    }
 }
 </style>
