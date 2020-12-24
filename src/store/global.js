a69d5481862aa2dd0bb45586495ee6523343fcbe
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-23 20:07:30 -0700

    :recycle: [refactor]: add addPostModal to valid modals

--- src/store/global.js
+++ src/store/global.js
@@ -37,6 +37,7 @@ export const actions = {
          "AddQuestionModal",
          "AddTopicModal",
          "AddSpaceModal",
+         "AddPostModal",
          "SharePostModal",
          "LoadingModal"
       ];
