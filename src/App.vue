e6cfcfa31e3c3d48e6446bd6b3af260fb295b86a
Mayowa Ojo <mayowaojo.e@gmail.com>
2021-01-02 20:57:45 -0700

    :sparkles: [feat]: create temporary fallback for mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
-   <div id="app" :class="{'py-12': currentRoute != 'Authentication'}">
+   <div
+      id="app"
+      :class="{'py-12': currentRoute != 'Authentication'}"
+      v-if="!isMobile"
+   >
       <Navigation v-if="currentRoute != 'Authentication'"/>
       <main class="wrapper mx-auto" ref="q-container">
       <router-view />
       </main>
       <ModalWrapper />
    </div>
+   <div class="flex flex-col items-center justify-center px-8 mt-20" v-else>
+      <h1 class="text-k-21 text-kora-red1" style="font-family: 'Rye', cursive;">Kora</h1>
+      <p class="text-k-13 text-kora-light1 font-medium mt-4">Apologies. This app is not yet optimized for mobile view as it is currently a work in progress.</p>
+      <p class="text-k-13 text-kora-light1 font-normal mt-2">Kindly open this app on desktop for the best experience.</p>
+   </div>
 </template>
 
 <script>
+import { isMobile } from "mobile-device-detect";
 // @ is an alias to /src
 import Navigation from "@/components/Navigation.vue";
 import ModalWrapper from "@/components/ModalWrapper.vue";
@@ -26,7 +36,8 @@ export default {
       },
       status: function() {
          return this.$store.state.status;
-      }
+      },
+      isMobile: () => isMobile
    },
    watch: {
       currentRoute: function(newVal) {
