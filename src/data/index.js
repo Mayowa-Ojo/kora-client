4f1d99cc3643784472829b6e35f69675b822964a
Mayowa Ojo <mayowaojo.e@gmail.com>
2021-01-03 16:03:38 -0700

    :sparkles: [feat]: create static data

---
+++ src/data/index.js
@@ -0,0 +1,53 @@
+export const spaces = [
+   {
+      name: "JS Learning Library",
+      img: "https://qph.fs.quoracdn.net/main-thumb-ti-2236-100-hflovidnrsqhevnpcdvqzywwsjluppwg.jpeg",
+   },
+   {
+      name: "Communityy",
+      img: "https://qsf.fs.quoracdn.net/-4-ans_frontend_assets.images.tribes.defaults.space_icon_purple.png-26-6ea9822273dc841e.png",
+   },
+   {
+      name: "Algorithms",
+      img: "https://qph.fs.quoracdn.net/main-thumb-ti-1577594-100-sjaxdzpcnnwrcgvmgwwcpknnnhoksilh.jpeg",
+   },
+   {
+      name: "Mt. Myoboku",
+      img: "https://qph.fs.quoracdn.net/main-thumb-ti-1574650-100-egwjyfyfyqzcfxihzpavoinbedrzokyz.jpeg",
+   },
+   {
+      name: "Code",
+      img: "https://qph.fs.quoracdn.net/main-thumb-ti-1122-100-ookytsozqmggkanzqpqojaagnefubsff.jpeg",
+   },
+   {
+      name: "Programmer's World",
+      img: "https://qph.fs.quoracdn.net/main-thumb-ti-694310-100-jtywjhonwrxkqfthxqasqnwywkghqkjh.jpeg",
+   },
+   {
+      name: "Art for Engineers",
+      img: "https://qph.fs.quoracdn.net/main-thumb-ti-1577050-100-hdfppcmpkeiniqpnrcpznowaxzmxplhv.jpeg",
+   },
+   {
+      name: "The Programming Hub",
+      img: "https://qph.fs.quoracdn.net/main-thumb-ti-1580821-100-zvmdxqgbaulpjtybcafxljjsmfusqyff.jpeg"
+   }
+]
+
+export const topics = [
+   {
+      name: "Web Development",
+      img: "https://qph.fs.quoracdn.net/main-thumb-t-1162-100-4eoeIXYIiEDd6u22xlsrUnul3iiHgejb.jpeg"
+   },
+   {
+      name: "User Experience",
+      img: "https://qph.fs.quoracdn.net/main-thumb-t-4052-100-khhbtngfzevsrbpzrjoartplpmusqwlx.jpeg"
+   },
+   {
+      name: "Adobe Photoshop",
+      img: "https://qph.fs.quoracdn.net/main-thumb-t-4218-100-sllhkecdvfwvjsqfkxlwvmgbgcuovttz.jpeg"
+   },
+   {
+      name: "Graphic design",
+      img: "https://qph.fs.quoracdn.net/main-thumb-t-3813-100-fpxbgswoojjanesdbnutiefvqfaclriu.jpeg"
+   }
+]
